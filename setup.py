5c36628331195e3a6f75edbaf3d7c6cef4b3f78d
Jakub Bąk <jakub.bak@dcx.bz>
2016-09-16 17:08:12 +0200

    Start trying out click lib for CLI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='Medicover-CLI',
+    version='1.0',
+    py_modules=['cli'],
+    install_requires=[
+        'requests', 'bs4', 'click'
+    ],
+    entry_points={
+        'console_scripts': ['medicover=cli:hello']
+    }
+)
